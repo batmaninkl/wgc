1dd233e2db6d3a7572ca072d47afb739a69e191a
batmaninkl <adamramlan@gmail.com>
2022-05-11 16:52:55 +0800

    Initial commit

---
+++ public/slider-wheel.js
@@ -0,0 +1,21 @@
+/* ******************************************* */
+/*  Simple Slider Wheel                        */
+/*  -------------------                        */
+/*  Copyright                                  */
+/*     Mejias David   -> Info-DVD aka M-DVD    */
+/* ******************************************* */
+/*  MIT License                                */
+/* ******************************************* */
+
+		$(document).ready(function(){
+			heightSlide = $("#slide-04").outerHeight(true) / 2;
+			heightLastSlide = $("#slide_04").offset().top ;
+			$("body").scrollspy({target: '.dvd_fixed_slider', offset: heightSlide });
+		});
+
+		$(window).on("scroll", function () {
+			var scrollTop = $(window).scrollTop();
+			var attrPosition = (scrollTop > heightLastSlide) ? 'static' : 'absolute';
+			$("#slide-04").css({ position: attrPosition });
+		});
+
