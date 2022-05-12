8a62ed8b33888a9c61f88538e1ea853cc9ee934b
batmaninkl <adamramlan@gmail.com>
2022-05-12 11:14:22 +0800

    Added folders and Ashrafs work

---
+++ test3/index.js
@@ -0,0 +1,17 @@
+function copy1() {
+	document.getElementById("result1").style.opacity = "1";
+	document.getElementById("result2").style.opacity = "0.3";
+	document.getElementById("result3").style.opacity = "0.3";
+}
+
+function copy2() {
+	document.getElementById("result1").style.opacity = "0.3";
+	document.getElementById("result2").style.opacity = "1";
+	document.getElementById("result3").style.opacity = "0.3";
+}
+
+function copy3() {
+	document.getElementById("result1").style.opacity = "0.3";
+	document.getElementById("result2").style.opacity = "0.3";
+	document.getElementById("result3").style.opacity = "1";
+}
